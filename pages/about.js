959f7cce6130a382b8cf318b379ad2d53939ae62
ebimo21 <ebi4jah15@gmail.com>
2022-05-10 09:56:40 +0100

    testing reading from json file

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,35 @@
 import React from 'react'
 
-const about = () => {
+const about = ({users}) => {
   return (
-    <div>about</div>
+    <div>
+        <h2>Users</h2>
+        {console.log(users)}
+
+        <ul>
+            {users.map((user) =>(
+                <li key={user.id}>
+                    <p>{user.name}</p>
+                    <p>{user.username}</p>
+                    <a href={"https://www."+user.website}>{user.website}</a>
+                </li>
+
+            ))}
+            
+        </ul>
+    </div>
   )
+}
+
+export const getStaticProps = async() =>{
+    const res = await fetch('http://jsonplaceholder.typicode.com/users?_limit=5')
+    const users = await res.json();
+
+    return{
+        props: {
+            users,
+        }
+    }
 }
 
 export default about
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -14,6 +15,8 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {/* <link href='/about' /> */}
+          <a href='/about'> About</a>
         </h1>
 
         <p className={styles.description}>
